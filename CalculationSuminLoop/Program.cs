d621675544b01a4633dd7c2ab53b1344177367e4
Irina Voronina <irinaworonina@gmail.com>
2022-04-03 13:59:35 +0300

    add my fourth c# project

---
+++ CalculationSuminLoop/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CalculationSuminLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //программа генерирует 10 случайных чисел (от 1 до 10), выводит их значение на экран
+            //программа подсчитывает сумму этих чисел и выводит сумму на экран
+
+            Console.WriteLine("Посчитать сумму десяти случайных чисел");
+
+            Random rnd = new Random();
+            int sum = 0;
+
+            for (int i = 1; i < 11; i++)
+            {
+                int myRandom = rnd.Next(1, 11);
+                sum = sum + myRandom;
+                Console.WriteLine($"{i} Random nr: {myRandom}");
+            }
+            Console.WriteLine($"Summa = {sum}", sum);
+
+
+
+
+        }
+
+
+    }
+           
+    
+}
